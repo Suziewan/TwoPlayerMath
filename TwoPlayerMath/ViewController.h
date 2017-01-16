68ef55e35cc36b343faa4b6e5b130594c36052e3
Suziewan <angielinton@me.com>
2017-01-16 16:21:34 -0500

    Initial Commit

---
+++ TwoPlayerMath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwoPlayerMath
+//
+//  Created by Angie Linton on 2017-01-16.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TwoPlayerMath/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwoPlayerMath
+//
+//  Created by Angie Linton on 2017-01-16.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
